812341c13f56b8c2fdb80beef19e179cc7a9fed1
Pawan <p.k.t.7042@gmail.com>
2021-05-20 00:07:23 +0530

    creating New Modal and then Converting it into ticket

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 let filtersDiv = document.querySelectorAll(".filters-div div");
+let add = document.querySelector(".add");
 let lower = document.querySelector(".lower");
 
-//creating an object
+//creating a list of object
 let FilterList = [{name : "red" , color: "red"},
-                  {name : "yellow" , color: "yellow"},
+                  {name : "yellow" , color: "#f9ca24"},
                   {name : "green" , color: "green"},
                   {name : "black", color: "black"},
                  ];
 
 for(let i = 0; i<filtersDiv.length; i++)
 {
-    filtersDiv[i].addEventListener("click", cb);
+    filtersDiv[i].addEventListener("click", changeBgrndOnFilterClick);
 }
-
-function cb(e){
+//cb function
+function changeBgrndOnFilterClick(e){
     let clickedFilter = e.target.classList[0];
     for(let j = 0; j< FilterList.length; j++){
         if(FilterList[j].name == clickedFilter)
@@ -21,3 +22,52 @@ function cb(e){
     lower.style.background = lowerBackgroundColor;
     }
 }
+
+// adding new modal on the click on add button
+add.addEventListener("click",openNewModal);
+function openNewModal(){
+    let isModalPresent = document.querySelector(".modal");
+    if(isModalPresent)
+    {
+        return;
+    }
+    let newModal = document.createElement('div');
+    newModal.classList.add('modal');
+    newModal.innerHTML = ` <div class="modal-msg" data-typed = "false" contenteditable="true">
+    Enter Your Task
+    </div>
+    <div class="modal-filters-div">
+        <div class="red modal-filter"></div>
+        <div class="yellow modal-filter"></div>
+        <div class="green modal-filter"></div>
+        <div class="black modal-filter active-filter"></div>
+        </div>`;
+        lower.append(newModal);
+        let ModalMsgBox = newModal.querySelector(".modal-msg")
+        ModalMsgBox.addEventListener("click",clearText);
+        ModalMsgBox.addEventListener("keypress",ModalToTicketOnEnter);
+
+    }
+//cb function To clear the text of ModalMsgBox 
+function clearText(e){
+    if(e.target.getAttribute("data-typed") == "true"){
+        return;
+    }
+    e.target.innerHTML = "";
+    e.target.setAttribute("data-typed","true");
+}
+
+//cb function to Create a new Ticket on clicking "Enter" in Modalmsg
+function ModalToTicketOnEnter(e){
+    if(e.key == "Enter"){
+        // e.target.parenNode.remove();
+        let newTicket = document.createElement("div");
+        newTicket.classList.add("ticket");
+        newTicket.innerHTML = `<div class="ticket-color"></div>
+        <div class="ticket-id">abc123</div>
+        <div class="task" contenteditable="true">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, autem eaque reiciendis non velit temporibus culpa laborum, neque fugit sed harum vel nam vitae blanditiis possimus libero sint nisi saepe!</div>`
+        lower.append(newTicket);
+        e.target.parentNode.remove();
+    }
+}
+
